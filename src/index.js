718af5e2ac0f19e1b7efc8bb956ac1e369844392
Rafael Santana <rafaelsantana7213@gmail.com>
2023-04-17 19:34:02 -0300

    feat: creat chat

---
+++ src/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const express_2 = require("express");
+const app = (0, express_1.default)();
+app.use(express_1.default.static(__dirname + "/public"));
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
+io.on("connection", (socket) => {
+    io.to(socket.id).emit("Message:", "You are connected");
+    socket.on("message", (data) => {
+        socket.broadcast.emit("message", data);
+    });
+});
+const route = (0, express_2.Router)();
+app.use(express_1.default.json());
+route.get('/', (req, res) => {
+    res.render("index.html");
+});
+app.use(route);
+server.listen(3333, () => 'server running on port 3333');
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from 'cors';
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+app.use(cors())
+
+app.use(express.static(__dirname + "/public"));
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  io.to(socket.id).emit("Message:", "You are connected");
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);
+  })
+})
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.render("index.html")
+})
+
+app.use(route)
+
+
+server.listen(3333, () => 'server running on port 3333')
